7f6c8ed72cf31f149ab6ea305077523e37aed148
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-07-27 20:06:57 +0200

    update holamundo

--- java1-practise/src/HolaMundo/HolaMundo.java
+++ java1-practise/src/HolaMundo/HolaMundo.java
@@ -3,7 +3,31 @@ public class HolaMundo {
     public static void main(String[] args) {
         String saludar = "Hola Mundo";
         System.out.println(saludar);
-        
+        System.out.println("saludar = "+ saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = "+ numero);
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = "+ numero2);
+        }
+
+        // tipo flexible integer
+        var numero3 = 15;
+
+        // tipo flexible  String al ir en comillas 
+        var numero4 = "15";
+
+        String nombre;
+        nombre = "Andres";
+        if(numero>8){
+            nombre="Juan";
+        }
+        System.out.println("nombre = "+ nombre);
+
+
     }
 
 }
