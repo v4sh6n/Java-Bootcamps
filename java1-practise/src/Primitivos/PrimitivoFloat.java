296ae1e8a12775125a647f5e98dcb41ef18d1591
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-08-10 18:19:09 +0200

    float lesson practise

---
+++ java1-practise/src/Primitivos/PrimitivoFloat.java
@@ -0,0 +1,23 @@
+public class PrimitivoFloat {
+    static float varFlotante; //por defecto es valor 0.0
+    public static void main(String[] args) {
+        
+        float realFloat = 0.00000000000000120f;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("tipo Float corresponde en byte a " + Float.BYTES);
+        System.out.println("tipo Float corresponde en bites a " + Float.SIZE);
+        System.out.println("valor máx de un Float " + Float.MAX_VALUE);
+        System.out.println("valor min de un Float " + Float.MIN_VALUE);
+        
+        double realDouble = 3.487487;
+        System.out.println("realFloat = " + realDouble);
+        System.out.println("tipo Double corresponde en byte a " + Double.BYTES);
+        System.out.println("tipo Double corresponde en bites a " + Double.SIZE);
+        System.out.println("valor máx de un Double " + Double.MAX_VALUE);
+        System.out.println("valor min de un Double " + Double.MIN_VALUE);
+
+        //var varFlotante = 3.1416f; // cuando es local la variable debe inicializarse siempre
+        System.out.println("varFlotante = " + varFlotante);
+
+    }
+}
