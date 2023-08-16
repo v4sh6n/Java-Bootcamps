391c4d05d9472f1fd7782c082b1b76bfc397ba28
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-08-16 19:56:45 +0200

    characters practise

---
+++ java1-practise/src/Characters/Caracteres.java
@@ -0,0 +1,17 @@
+public class Caracteres {
+
+    public static void main(String[] args) {
+        char caracterer = '\u8840';
+        char decimal = 64;
+        char caracterer2 = '0';
+        char caracterer3 = 'A';
+
+        System.out.println("Caracter = " + caracterer3);
+        System.out.println("Caracter = " + caracterer2);
+        System.out.println("Caracter =" + caracterer);
+        System.out.println("Caracter =" + decimal);
+
+        
+    }
+
+}
