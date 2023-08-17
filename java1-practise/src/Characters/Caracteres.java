76d3079d229dc87176ddc8dae710c028f5f0f44f
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-08-17 19:13:24 +0200

    update class

--- java1-practise/src/Characters/Caracteres.java
+++ java1-practise/src/Characters/Caracteres.java
@@ -1,17 +1,24 @@
 public class Caracteres {
 
     public static void main(String[] args) {
-        char caracterer = '\u8840';
+        char caracter = '\u0040'; //unicode character
         char decimal = 64;
         char caracterer2 = '0';
         char caracterer3 = 'A';
+        char simbolo = '@';
 
-        
+        System.out.println("Simbolo = " + simbolo);
         System.out.println("Caracter = " + caracterer3);
         System.out.println("Caracter = " + caracterer2);
-        System.out.println("Caracter =" + caracterer);
-        System.out.println("Caracter =" + decimal);
+        System.out.println("Caracter = " + caracter);
+        System.out.println("Caracter = " + decimal);
+        System.out.println("decimal == caracter : " + (decimal == caracter));
 
+        System.out.println("tipo char corresponde en byte a " + Character.BYTES);
+        System.out.println("tipo char corresponde en bites a " + Character.SIZE);
+        System.out.println("valor máx de un char " + Character.MAX_VALUE);
+        System.out.println("valor min de un char " + Character.MIN_VALUE);
+        
         
     }
 
