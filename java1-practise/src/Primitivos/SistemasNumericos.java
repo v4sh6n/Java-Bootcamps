4385169c962d15ef29564cf8a05556083f2ad5fc
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-08-24 18:34:37 +0200

    update new class

---
+++ java1-practise/src/Primitivos/SistemasNumericos.java
@@ -0,0 +1,18 @@
+public class SistemasNumericos {
+
+    public static void main(String[] args) {
+
+        int numeroDecimal = 500;
+
+        System.out.println( "numero binario " +numeroDecimal+" = " + Integer.toBinaryString(numeroDecimal));
+         
+        int numeroBinario = 0b111110100;
+        System.out.println("111110100 de binario a numerico :"+ numeroBinario);
+         
+        System.out.println("numero hexadecimal de "+ Integer.toHexString(numeroBinario));
+        
+        int numeroHex = 0x1f4; //hexadecimal to decimal
+        System.out.println("numeroHex = "+ numeroHex);
+    }
+    
+}
