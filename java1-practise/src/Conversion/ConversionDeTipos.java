1bcf3535ded626ddf2fb360dd5a46d3e38625341
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-11-20 18:37:44 +0100

    update conversion

---
+++ java1-practise/src/Conversion/ConversionDeTipos.java
@@ -0,0 +1,19 @@
+package Conversion;
+
+public class ConversionDeTipos {
+    public static void main(String[] args) {
+        String numeroStr = "58";
+
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("numeroInt = " + numeroInt);
+
+        String realStr = "9757,48";
+        double realDouble = Double.parseDouble(realStr);
+        System.out.println("realDouble = " + realDouble);
+
+        String logicoStr = "true";
+        boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
+        System.out.println("logicoBoolean = " + logicoBoolean);
+    }
+
+}
