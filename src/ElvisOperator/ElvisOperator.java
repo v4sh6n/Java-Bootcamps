817116a64abc3d54b003d6372954f2c7e0525b21
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2024-02-22 18:12:46 +0100

    add elvis operator

---
+++ src/ElvisOperator/ElvisOperator.java
@@ -0,0 +1,15 @@
+package ElvisOperator;
+
+public class ElvisOperator {
+
+    public static void main(String[] args) {
+
+    //elvis operator simplifies if else
+    var result = (3>2) ? true : false;
+    //if 3 is greater than 2 result is true
+    System.out.println("Result: "+result);
+
+    
+    
+}
+}
