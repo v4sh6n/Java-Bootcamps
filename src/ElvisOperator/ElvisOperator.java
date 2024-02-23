b2b67e66d3e42ed8e4d562bd0a9584a420f7f77e
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2024-02-23 16:48:54 +0100

    update ElvisOperator.java

--- src/ElvisOperator/ElvisOperator.java
+++ src/ElvisOperator/ElvisOperator.java
@@ -3,13 +3,22 @@ package ElvisOperator;
 public class ElvisOperator {
 
     public static void main(String[] args) {
-
+    //var infers type
+    //with variable type Boolean
     //elvis operator simplifies if else
     var result = (3>2) ? true : false;
     //if 3 is greater than 2 result is true
     System.out.println("Result: "+result);
 
+    //with variable type Strings
+    var result2 = (3>2) ? "true" : "false";
+    System.out.println("Result: "+result2);
+
+    //with both boolean & String
+    var result3 = (3>2) ? "true" : false;
+    System.out.println("Result: "+result3);
+
     
-    
+
 }
 }
