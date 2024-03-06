b7cfa9ddc75c23e25dc75db2d0a23fd2015697c6
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2024-03-06 17:31:46 +0100

    add conditionals2.java

---
+++ src/Conditionals/Conditionals2.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+public class Conditionals2 {
+    public static void main(String[] args) {
+        var number = 3;
+        if (number == 1){
+            System.out.println("number is : 1");
+        }else if(number == 2){
+            System.out.println("number is : 2");
+        }else if(number == 3){
+            System.out.println("number is : 3");
+        }else{
+            System.out.println("unknown value");
+        }
+    }
+    
+}
